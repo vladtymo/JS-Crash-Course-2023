0afda1111e5c895cf1d00c9aa414bbe14bba344c
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-04-18 17:02:36 +0300

    Events, forms example. Using Bootstrap library.

---
+++ 04_Events/main.js
@@ -0,0 +1,44 @@
+const products = [];
+
+const tableBody = document.querySelector('#product-table tbody');
+// add product to the table
+function addProductToTable(product) {
+
+    tableBody.innerHTML += `<tr>
+                                <th scope="row">${product.id}</th>
+                                <td>${product.name}</td>
+                                <td>${product.price}$</td>
+                            </tr>`;
+}
+
+// get product from form
+function readProduct() {
+    const nameInput = document.getElementById('nameInput');
+    const priceInput = document.getElementById('priceInput');
+
+    return {
+        id: products.length + 1,
+        name: nameInput.value,
+        price: priceInput.value
+    };
+}
+
+// ----------- events -----------
+const addBtn = document.getElementById('add-product-btn');
+const clearBtn = document.getElementById('clear-all-btn');
+
+// add event handler
+addBtn.onclick = () => {
+    // ... code ...
+    console.log('Clicked!');
+
+    const newProduct = readProduct();
+    products.push(newProduct);
+
+    addProductToTable(newProduct);
+}
+
+clearBtn.onclick = () => {
+    tableBody.innerHTML = '';
+    products.splice(0);
+}
